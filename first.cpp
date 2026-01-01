301c580070ba64dd3f31698c6a0f000259c505d3
pranab-kr <pranabm406@gmail.com>
2026-01-02 01:04:38 +0530

    first

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout<<"Pranab kr Manna"<<endl;
+  cout<<"Age - 20"<<endl<<"Fav gaccha game - WUWA\n";
+  cout<<"sum of two 5 is - "<<5+5<<endl;
+
+}
