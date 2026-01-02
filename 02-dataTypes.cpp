c63a42202a25826565d31d511b11af9a6569d2a7
pranab-kr <pranabm406@gmail.com>
2026-01-02 13:03:07 +0530

    data types

---
+++ 02-dataTypes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// data types in C++
+
+  int a = 1, b= 3 , c = 23;
+
+  cout<<a<<endl<<b<<endl<<c<<endl;
+
+  float f1 = 2.34;
+
+  cout<<f1<<endl;
+
+  double F1 = 2.14343534;
+
+  cout<<F1<<endl;
+
+  char C1 = 'p';
+
+  cout<<C1<<endl;
+
+  bool B1 = true , B2 = 0;
+
+  cout<<B1<<endl<<B2<<endl;
+
+}
