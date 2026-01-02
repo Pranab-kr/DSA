75f798bc2fc05691cf248ffc3526caab281aa65d
pranab-kr <pranabm406@gmail.com>
2026-01-02 21:17:14 +0530

    if-else

---
+++ if-else/01-pack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int pack;
+
+  cout<<"Enter ur salary package - ";
+  cin>>pack;
+
+  if (pack > 10 )
+    cout<<"Accepted";
+
+   else
+    cout<<"Rejected";
+
+}
