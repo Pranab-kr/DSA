d6e68106d51ad941857791e06e22169238817dd1
pranab-kr <pranabm406@gmail.com>
2026-01-02 20:41:49 +0530

    input

---
+++ 03-input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int a ,b ;
+
+  cout<<"Enter Ur first Number - ";
+
+  cin>>a;
+
+  cout<<"Entr ur second Number - ";
+
+  cin>>b;
+
+  cout<<"Sum of two numbers are - "<<a+b<<endl;
+
+
+}
