6cbb8e6e521287e5f2944429b74b4676425e6e2d
Pranab-kr <pranabm406@gmail.com>
2026-07-24 00:48:18 +0530

    L-41/2

---
+++ String/L-41/leet/1.longest-subStr.cpp
@@ -0,0 +1,24 @@
+// 3. Longest Substring Without Repeating Characters
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+//
+// TC -> O(n) , SC -> O(1)
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    vector<bool> count(256, 0);
+    int first = 0, second = 0, len = 0;
+
+    while (second < s.size()) {
+      while (count[s[second]]) { // Repeating char remove
+        count[s[first]] = 0;
+        first++;
+      }
+      count[s[second]] = 1;
+      len = max(len, second - first + 1);
+      second++;
+    }
+
+    return len;
+  }
+};
